0a6334b0d418b0d5290abe82b94e56e612b46418
Daniel Tomcej <daniel.tomcej@gmail.com>
2017-01-29 11:18:10 -0700

    update byte array to string

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	//to achieve 64 bits of entropy, we calculate the entropy per symbol of our set
 	character_entropy := math.Log2(float64(len(characters)))
-	min_chars := math.Floor((64/character_entropy) + 0.5)
+	min_chars := int(math.Floor((64/character_entropy) + 0.5))
 
 	//initialize random number generator
 	source := rand.NewSource(time.Now().UnixNano())
@@ -28,14 +28,21 @@ func main() {
 		var length int
 		fmt.Sscan(scanner.Text(), &length)
 		fmt.Printf("Entered Length: %v\n", length)
-		if length >= 12 {
+		if length >= min_chars {
 			password := make([]byte, length)
 			for i := 0; i < length; i++ {
 				password[i] = characters[gen.Intn(len(characters))]
 			}
+			fmt.Printf("System Generated Password: %v\n", string(password[:]))
+		}	else {
+			//password length not long enough
+			fmt.Printf("Password length not long enough. Generating passwords with minimum length: %v\n", min_chars)
+			length = min_chars
+			password := make([]byte, length)
+			for i := 0; i < length; i++ {
+				password[i] = characters[gen.Intn(len(characters))]
+			}
+			fmt.Printf("System Generated Password: %v\n", string(password[:]))
 		}
 	}
-
-//		fmt.Println("Random Keys:")
-//		key := gen.Intn(26)
 }
